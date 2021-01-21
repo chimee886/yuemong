10cedc876873b1095a47e8cb4a477f91fd5f8c0e
chimee <chimee886@gmail.com>
2021-01-22 00:37:47 +0800

    网易源音乐播放功能

--- src/js/discovery-news-ong.js
+++ src/js/discovery-news-ong.js
@@ -25,7 +25,8 @@
     }
     let model = {
         newSongList: [],
-        getNewSongList() {
+        index: 0,
+        getNewSongList() { //获取首页新歌列表
             return new Promise(function(resolve, reject) { //获取发现页最新歌曲列表
                 $.ajax({
                     type: 'get',
@@ -34,7 +35,7 @@
                         //将获取到的数据处理后存储到本地
                         console.log(response)
                         model.newSongList = response.result.map((list) => {
-                            return { id: list.id, cover: list.picUrl + '?param=500y500', name: list.name, singer: list.song.artists[0].name, album: list.song.album.name }
+                            return { source: 1, id: list.id, cover: list.picUrl + '?param=500y500', name: list.name, singer: list.song.artists[0].name, album: list.song.album.name }
                         })
                         console.log(model.newSongList)
                         resolve()
@@ -49,10 +50,77 @@
             this.model = model
             this.view.init()
             this.model.getNewSongList().then(() => {
+                //将歌曲渲染到发现页面
                 for (let i = 0; i < this.model.newSongList.length; i++) {
                     this.view.$el.append(this.view.render(this.model.newSongList[i]))
                 }
+                console.log(playList)
+                console.log(this.model.newSongList)
+                console.log('this.model.newSongList')
+                this.playNeteaseSong('#netEaseNewMusic', this.model.newSongList)
+
+
+
+
+
+                //this.changeSongInfo()
             })
+        },
+        playNeteaseSong(ul, model) {
+            $(ul).on('click', 'li', (e) => {
+                //let songId = e.currentTarget.dataset.songId
+                console.log(model)
+                for (let i = 0; i < model.length; i++) {
+                    if (!model[i].url) {
+                        //在找到的songId里面加上url属性
+                        console.log(i)
+                        console.log(model[i].id)
+                        console.log(getNeteaseSongUrl(model[i].id))
+                        model[i].url = getNeteaseSongUrl(model[i].id)
+                    } else {
+                        console.log('url有了')
+                        break
+                    }
+                }
+
+                let li = e.currentTarget
+
+                //获取dom里面的歌曲id
+                let id = Number($(li).attr('data-song-id'))
+                let index
+                console.log(id)
+
+                //根据id在数据里面查找url
+                for (let i = 0; i < this.model.newSongList.length; i++) {
+                    //console.log(this.model.newSongList[i].id)
+                    if (this.model.newSongList[i].id === id) {
+                        index = i
+                        break
+                    }
+                }
+                console.log(index)
+                this.changeSongInfo(this.model.newSongList[index])
+                $('#audio')[0].play()
+                isPlay = true
+                window.isPlaying()
+            })
+        },
+        changeSongInfo(data) { //将当前播放歌曲的数据渲染到dom
+
+            $('#player').find('.player-name').text(data.name)
+            $('#player').find('.player-singer').text(data.singer)
+                //替换audio标签的src
+            let audio = $('#audio')
+            audio.attr('src', data.url)
+                //替换2处封面
+            let allCover = $('.player-cover>img')
+            for (let i = 0; i < allCover.length; i++) {
+                $(allCover[i]).attr('src', data.cover)
+            }
+            //给当前播放的歌曲列表添加背景色
+            //this.view.changePlayStatus(data.id)
+            //this.generateUrl(data.id) //生成当前歌曲的url地址
+
         }
     }
     controller.init(view, model)
--- src/js/musicController.js
+++ src/js/musicController.js
@@ -1,7 +1,7 @@
 var isPlay = false
 var currentSongId
 
-
+//检测歌曲是否播放，控制图标与cover状态
 window.isPlaying = function() {
     console.log(isPlay)
     let audio = $('#audio')
@@ -22,4 +22,24 @@ window.isPlaying = function() {
 }
 var playList = {}
 var playMode //播放模式[singleLoop/listOrder/listRandom]
-var playListIndex = []
+var playListIndex = [] //播放历史记录角标
+
+
+//根据当前点击的歌曲，获取歌曲的url
+function getNeteaseSongUrl(id) {
+    let songurl = null
+    $.ajax({
+        type: 'get',
+        url: 'http://192.168.31.135:3000/song/url?id=' + id,
+        async: false,
+        success: function(res) {
+            //在model里面的歌曲里面查找songId
+            //在找到的songId里面加上url属性
+            songurl = res.data[0].url
+        },
+        error: function(req) {
+            console.log(JSON.stringify(req))
+        }
+    })
+    return songurl
+}
